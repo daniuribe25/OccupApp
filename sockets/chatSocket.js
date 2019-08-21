5959b177849f8adc546324ea592bf4afbdec0048
Daniel <dani.uribe25@gmail.com>
2019-08-20 21:29:41 -0500

    last fix

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -3,7 +3,7 @@
 	chatSocket.saveMessage = async (socket, mobileSockets, { text, user, user2, createdAt, chatId }) => {
 		const messageResp = await chatRepo.addMessage(chatId, { text, createdAt, user	});
 		// emit response to sender
-		socket.emit('incomingMessage', { user, user2,  messageResp, chatId, sender: true });
+		socket.emit('incomingMessageChat', { user, user2,  messageResp, chatId, sender: true });
 		// emit response to receiver
 		const resp = { user, user2, messageResp, chatId, sender: false };
 		socket.to(mobileSockets[user2]).emit('incomingMessageApp', resp);
