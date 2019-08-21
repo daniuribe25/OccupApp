cd043753b1239a6783ddff88d769b0044475daba
Daniel <dani.uribe25@gmail.com>
2019-08-20 21:28:48 -0500

    add more emiiter to app

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -2,16 +2,13 @@
 
 	chatSocket.saveMessage = async (socket, mobileSockets, { text, user, user2, createdAt, chatId }) => {
 		const messageResp = await chatRepo.addMessage(chatId, { text, createdAt, user	});
-		console.log('mobileSockets: ', mobileSockets);
-		console.log('user1: ', user);
-		console.log('user2: ', user2);
-		console.log('message: ', text);
 		// emit response to sender
 		socket.emit('incomingMessage', { user, user2,  messageResp, chatId, sender: true });
 		// emit response to receiver
-
-		
-		socket.to(mobileSockets[user2]).emit('incomingMessage', { user, user2, messageResp, chatId, sender: false });
+		const resp = { user, user2, messageResp, chatId, sender: false };
+		socket.to(mobileSockets[user2]).emit('incomingMessageApp', resp);
+		socket.to(mobileSockets[user2]).emit('incomingMessageList', resp);
+		socket.to(mobileSockets[user2]).emit('incomingMessageChat', resp);
 	};
 
 })(
