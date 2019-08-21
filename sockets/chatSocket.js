fa0ed1000463dae1f21b4e33ffa3f26b96038acb
Daniel <dani.uribe25@gmail.com>
2019-08-20 21:47:33 -0500

    add logs

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -3,12 +3,17 @@
 	chatSocket.saveMessage = async (socket, mobileSockets, { text, user, user2, createdAt, chatId }) => {
 		const messageResp = await chatRepo.addMessage(chatId, { text, createdAt, user	});
 		// emit response to sender
-		socket.emit('incomingMessageChat', { user, user2,  messageResp, chatId, sender: true });
+		socket.emit('incomingMessage', { user, user2,  messageResp, chatId, sender: true });
 		// emit response to receiver
 		const resp = { user, user2, messageResp, chatId, sender: false };
 		socket.to(mobileSockets[user2]).emit('incomingMessageApp', resp);
 		socket.to(mobileSockets[user2]).emit('incomingMessageList', resp);
 		socket.to(mobileSockets[user2]).emit('incomingMessageChat', resp);
+
+		console.log('mobileSockets: ', mobileSockets);
+		console.log('user1: ', user);
+		console.log('user2: ', user2);
+		console.log('message: ', text);
 	};
 
 })(
